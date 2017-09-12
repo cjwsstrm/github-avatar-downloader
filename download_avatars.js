143a5520d6b1679015796806ac9e8745004aa374
cjwsstrm <cjwesstrom@gmail.com>
2017-09-12 00:01:21 +0000

    created intial getRepoContributors() function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+
+var GITHUB_USER = "cjwsstrm";
+var GITHUB_TOKEN = "8b13fe355f1c2b0fc4e4244b7dffe3a972179f27";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
+}
